2a79848aa1968fdf57ee03e7706534b3eacb909b
Cruz Barnum <cruzbarnum@gmail.com>
2016-10-24 18:56:15 -0700

    Added method to help calculate positions

--- core/screen.py
+++ core/screen.py
@@ -7,11 +7,22 @@ import os
 
 
 screen = None
+dims = None
+wh = None
 
 
 def initiate(dim, texdim): #dim and texdim both tuples of ints
     global screen
+    global dims
+    global wh
+    dims = dim
+    wh = texdim
     screen = display.set_mode((dim[0] * texdim[0], dim[1] * texdim[1]))
+
+
+def calcpos(raw):
+    return raw[0] * dims[0], raw[1] * dims[1]
+
 
 # initiate((64 + 32, 32 + 4), (12, 18))
 # f = Font('../fonts/AS.png')
